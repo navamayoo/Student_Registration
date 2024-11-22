4db64941d7af57ddac0016da284c863f14632d68
Mayooran <mayoorannavam@gmail.com>
2024-11-22 17:18:40 +0530

    Update Program.cs

--- Student_Registration/Program.cs
+++ Student_Registration/Program.cs
@@ -1,5 +1,9 @@
 var builder = WebApplication.CreateBuilder(args);
 
+#region Database Configure
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<AppDbContext>(options =>options.UseSqlServer(connectionString));
+#endregion
 // Add services to the container.
 
 builder.Services.AddControllers();
