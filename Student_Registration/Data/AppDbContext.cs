12babcfc592c1ad71e4d32a72e81d896eabafe56
navamayoo <navamayoo@gmail.com>
2024-11-22 17:15:38 +0530

    Update DB Contest

---
+++ Student_Registration/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Student_Registration.Module;
+
+namespace Student_Registration.Data
+{
+    public class AppDbContext :DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) :base(options) 
+        { 
+        }  
+
+        DbSet<Student> Students { get; set; }
+        
+    }
+}
---
+++ Student_Registration/Module/Student.cs
@@ -0,0 +1,14 @@
+﻿namespace Student_Registration.Module
+{
+    public class Student
+    {
+        public int id { get; set; }
+        public string? name { get; set; }
+        public int age { get; set; }
+        public string? address { get; set; }
+        public string? gender { get; set; }
+        public string? phoneNum { get; set; }
+
+
+    }
+}
